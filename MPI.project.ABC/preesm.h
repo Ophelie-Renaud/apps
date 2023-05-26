6474895dc43ccd175e84e03ab9eea41f475df1a1
Ophelie-Renaud <ophelie.renaud@insa-rennes.fr>
2023-05-26 17:24:52 +0200

    Signed-off-by: Ophelie-Renaud <ophelie.renaud@insa-rennes.fr>

---
+++ MPI.project.ABC/preesm.h
@@ -0,0 +1,39 @@
+/*
+	============================================================================
+	Name        : preesm.h
+	Author      : kdesnos
+	Version     : 1.0
+	Copyright   : CECILL-C, IETR, INSA Rennes
+	Description : Usefull declarations for all headers used in preesm.
+	============================================================================
+*/
+
+#ifndef PREESM_H
+#define PREESM_H
+
+#define IN
+#define OUT
+
+#define NB_FRAME 3000
+// FPS: number of frame between two FPS measures
+#define FPS 10
+
+typedef unsigned char uchar;
+
+//#define PREESM_VERBOSE
+//#define PREESM_LOOP_SIZE 5
+
+#define PATH_LEFT "./dat/im0.ppm"
+#define PATH_RIGHT "./dat/im1.ppm"
+#define PATH_OUT "./out.ppm"
+#define PATH_TTF "./dat/DejaVuSans.ttf"
+
+#define PATH_TTF_FULL PROJECT_ROOT_PATH "/" PATH_TTF
+#define PATH_LEFT_FULL PROJECT_ROOT_PATH "/" PATH_LEFT
+#define PATH_RIGHT_FULL PROJECT_ROOT_PATH "/" PATH_RIGHT
+#define PATH_OUT_FULL PROJECT_ROOT_PATH "/" PATH_OUT
+
+
+
+
+#endif
